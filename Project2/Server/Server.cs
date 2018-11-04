eb84eb0ef10d713c2b3c08ed612bf57385d2ad76
Patrick Sewell <pssewell21@outlook.com>
2018-11-04 10:53:20 -0600

    Updating to look for specific message contents before esponding to a request from a client.  Adding DateTime to output.

--- Project2/Server/Server.cs
+++ Project2/Server/Server.cs
@@ -11,6 +11,9 @@ namespace Server
         // The default port number to listen for client connections
         private const int PORT = 800;
         private const int BUFFER_SIZE = 8192;
+        private const int TIMEOUT = 5000;
+
+        private const string REQUEST_STRING = "GetData";
 
         public Server()
         {
@@ -19,20 +22,31 @@ namespace Server
 
         private void startButton_Click(object sender, EventArgs e)
         {
-            outputTextBlock.Text += $"Server Started...{Environment.NewLine}";
+            outputTextBlock.Text += $"{DateTime.Now}: Server Started...{Environment.NewLine}";
+            outputTextBlock.Refresh();
 
             var udpClient = new UdpClient(PORT);
+            udpClient.Client.ReceiveTimeout = TIMEOUT;
 
             while (true)
             {
-                var responseData = Encoding.ASCII.GetBytes("SomeResponseData");
+                var responseData = Encoding.ASCII.GetBytes("Hello World");
 
                 var endPoint = new IPEndPoint(IPAddress.Any, 0);
                 var clientRequestData = udpClient.Receive(ref endPoint);
                 var clientRequest = Encoding.ASCII.GetString(clientRequestData);
 
-                outputTextBlock.Text += $"Received {clientRequest} from {endPoint.Address.ToString()}";
-                udpClient.Send(responseData, responseData.Length, endPoint);
+                if (clientRequest.Equals(REQUEST_STRING))
+                {
+                    outputTextBlock.Text += $"{DateTime.Now}: Received {clientRequest} from {endPoint.Address.ToString()}. Sending response{Environment.NewLine}{Environment.NewLine}";
+                    udpClient.Send(responseData, responseData.Length, endPoint);
+                }
+                else
+                {
+                    outputTextBlock.Text += $"{DateTime.Now}: Received {clientRequest} from {endPoint.Address.ToString()}. Unrecognized command{Environment.NewLine}{Environment.NewLine}";
+                }
+                
+                outputTextBlock.Refresh();
             }
         }
     }
