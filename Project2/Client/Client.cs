424bb77504b70e7b53b1d26425d63ae7f35eead4
Patrick Sewell <pssewell21@outlook.com>
2018-11-05 14:47:01 -0600

    **BROKEN** Intermeddiate commit for implementing data pull using sockets and network discovery using UDPClient

--- Project2/Client/Client.cs
+++ Project2/Client/Client.cs
@@ -12,9 +12,15 @@ namespace Client
         private const int TIMEOUT = 5000;
 
         private const string DISCOVER_MESSAGE = "DiscoverServer";
+        private const string GET_DATA_MESSAGE = "GetData";
         private const string INVALID_MESSAGE = "BadRequest";
 
+        private IPEndPoint _endPoint;
+        private Socket _socket;
         private IPAddress _serverIpAddress;
+
+        // This lock acts as a mutex to make sure only one action is being performed on the socket at a time
+        private readonly object _socketLock = new object();
 
         public Client()
         {
@@ -25,7 +31,7 @@ namespace Client
         {
             try
             { 
-                outputTextBlock.Text += $"{DateTime.Now}: Client Started...{Environment.NewLine}";
+                outputTextBlock.Text += $"{DateTime.Now}: Discovering Server...{Environment.NewLine}";
                 outputTextBlock.Refresh();
 
                 var udpClient = new UdpClient();
@@ -47,6 +53,7 @@ namespace Client
 
                     _serverIpAddress = endPoint.Address;
                     getDataButton.Enabled = true;
+                    discoverServerButton.Enabled = false;
                 }
                 catch (SocketException se)
                 {
@@ -72,7 +79,119 @@ namespace Client
 
         private void getDataButton_Click(object sender, EventArgs e)
         {
+            try
+            {
+                outputTextBlock.Text += $"{DateTime.Now}: Getting data from server...{Environment.NewLine}";
+                outputTextBlock.Refresh();
 
+                // Create a new socket to connect to the server with
+                _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP)
+                {
+                    ReceiveTimeout = TIMEOUT
+                };
+
+                var outboundPort = GetOutboundPort();
+
+                lock (_socketLock)
+                {
+                    _endPoint = new IPEndPoint(IPAddress.Any, outboundPort);
+
+                    // Bind using the port selected
+                    _socket.Bind(_endPoint);
+
+                    // Connect to the server using the IP entered by the user or the fallback address
+                    _socket.Connect(_serverIpAddress, PORT);
+
+                    if (_socket.Connected)
+                    {
+                        outputTextBlock.Text += $"Client is connected using port {outboundPort}{Environment.NewLine}";
+                    }
+                    else
+                    {
+                        outputTextBlock.Text += "Client is not connected";
+                    }
+
+                    outputTextBlock.Refresh();
+                }
+
+                var requestData = Encoding.ASCII.GetBytes(GET_DATA_MESSAGE);
+
+                try
+                {
+                    lock (_socketLock)
+                    {
+                        _socket.Send(requestData);
+                    }
+
+                    lock (_socketLock)
+                    {
+                        // Create a byte array to hold the received message
+                        var bytes = new byte[_socket.ReceiveBufferSize];
+
+                        // Receive the message
+                        var bytesRead = _socket.Receive(bytes);
+
+                        // Convert the message to a string (This application sends only strings between tiers)
+                        var serverResponse = Encoding.ASCII.GetString(bytes, 0, bytesRead);
+
+                        // Display received data
+                        outputTextBlock.Text += $"{DateTime.Now}: Received \"{serverResponse}\" from {_endPoint.Address.ToString()}. Ready to request data{Environment.NewLine}{Environment.NewLine}";
+                        outputTextBlock.Refresh();
+                    }
+
+                    //var serverResponseData = tcpClient.Receive(ref _endPoint);
+                    //var serverResponse = Encoding.ASCII.GetString(serverResponseData);
+                    //
+                    //outputTextBlock.Text += $"{DateTime.Now}: Received \"{serverResponse}\" from {endPoint.Address.ToString()}. Ready to request data{Environment.NewLine}{Environment.NewLine}";
+                    //
+                    //_serverIpAddress = endPoint.Address;
+                    //getDataButton.Enabled = true;
+                }
+                catch (SocketException se)
+                {
+                    if (se.SocketErrorCode == SocketError.TimedOut)
+                    {
+                        outputTextBlock.Text += $"{DateTime.Now}: Connection timed out waiting for a response from the server{Environment.NewLine}{Environment.NewLine}";
+                    }
+                    else
+                    {
+                        throw;
+                    }
+
+                }
+
+
+
+
+
+
+
+
+                //var tcpClient = new TcpClient();
+                //tcpClient.Client.ReceiveTimeout = TIMEOUT;
+
+                //var requestData = Encoding.ASCII.GetBytes(GET_DATA_MESSAGE);
+                ////var requestData = Encoding.ASCII.GetBytes(INVALID_MESSAGE);
+                //var endPoint = new IPEndPoint(IPAddress.Any, 0);
+
+                //tcpClient.Send(requestData, requestData.Length, new IPEndPoint(IPAddress.Broadcast, PORT));
+
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error Occurred");
+                Environment.Exit(1);
+            }
         }
+
+        // Picks a random port to send data from. Must be unique from other clients to prevent exceptions from occurring
+        private int GetOutboundPort()
+        {
+            var randomNumberGenerator = new Random();
+            return randomNumberGenerator.Next(51200, 52000);
+        }
+
+
     }
 }
--- Project2/Server/Server.cs
+++ Project2/Server/Server.cs
@@ -2,6 +2,8 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Server
@@ -9,11 +11,16 @@ namespace Server
     public partial class Server : Form
     {
         // The default port number to listen for client connections
-        private const int PORT = 800;
+        private const int NETWORK_DISCOVERY_PORT = 800;
+        private const int CLIENT_CONNECTION_PORT = 801;
         private const int BUFFER_SIZE = 8192;
         private const int TIMEOUT = 5000;
 
         private const string DISCOVER_MESSAGE = "DiscoverServer";
+        private const string GET_DATA_MESSAGE = "GetData";
+
+        private IPEndPoint _endPoint;
+        private Socket _socket;
 
         public Server()
         {
@@ -27,33 +34,113 @@ namespace Server
                 outputTextBlock.Text += $"{DateTime.Now}: Server Started...{Environment.NewLine}";
                 outputTextBlock.Refresh();
 
-                var udpClient = new UdpClient(PORT);
+                //var udpClient = new UdpClient(PORT);
 
+                // Create a new socket for clients to connect to
+                _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+
+                // Bind to the IP address found above using the default port
+                _socket.Bind(new IPEndPoint(IPAddress.Any, PORT));
+                _socket.Listen(BUFFER_SIZE);
+
+                Thread.Sleep(1000);
+
+                // Busy wait for clients to connect
                 while (true)
                 {
-                    var responseData = Encoding.ASCII.GetBytes("Hello World");
+                    // Accept incoming connections
+                    var socket = _socket.Accept();
 
-                    var endPoint = new IPEndPoint(IPAddress.Any, 0);
-                    var clientRequestData = udpClient.Receive(ref endPoint);
-                    var clientRequest = Encoding.ASCII.GetString(clientRequestData);
-
-                    if (clientRequest.Equals(DISCOVER_MESSAGE))
-                    {
-                        outputTextBlock.Text += $"{DateTime.Now}: Received \"{clientRequest}\" from {endPoint.Address.ToString()}. Sending response{Environment.NewLine}{Environment.NewLine}";
-                        udpClient.Send(responseData, responseData.Length, endPoint);
-                    }
-                    else
-                    {
-                        outputTextBlock.Text += $"{DateTime.Now}: Received \"{clientRequest}\" from {endPoint.Address.ToString()}. Unrecognized message{Environment.NewLine}{Environment.NewLine}";
-                    }
-
+                    outputTextBlock.Text += $"Client connected from {socket.RemoteEndPoint}{Environment.NewLine}";
                     outputTextBlock.Refresh();
+
+                    // Sets up async tasks that run on different threads to process messages from clients
+                    Task task = new Task(() => ProcessMessages(socket));
+                    task.Start();
+
+                    // If the task throws an exception, catch it and rethrow to handle it
+                    if (task.Exception != null)
+                    {
+                        throw task.Exception;
+                    }
                 }
+
+                //while (true)
+                //{
+                //    var responseData = Encoding.ASCII.GetBytes("Hello World");
+                //
+                //    var endPoint = new IPEndPoint(IPAddress.Any, 0);
+                //    var clientRequestData = udpClient.Receive(ref endPoint);
+                //    var clientRequest = Encoding.ASCII.GetString(clientRequestData);
+                //
+                //    if (clientRequest.Equals(DISCOVER_MESSAGE))
+                //    {
+                //        outputTextBlock.Text += $"{DateTime.Now}: Received \"{clientRequest}\" from {endPoint.Address.ToString()}. Sending response{Environment.NewLine}{Environment.NewLine}";
+                //        udpClient.Send(responseData, responseData.Length, endPoint);
+                //    }
+                //    else if (clientRequest.Equals(DISCOVER_MESSAGE))
+                //    {
+                //
+                //    }
+                //    else
+                //    {
+                //        outputTextBlock.Text += $"{DateTime.Now}: Received \"{clientRequest}\" from {endPoint.Address.ToString()}. Unrecognized message{Environment.NewLine}{Environment.NewLine}";
+                //    }
+                //
+                //    outputTextBlock.Refresh();
+                //}
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.ToString(), "Error Occurred");
                 Environment.Exit(1);
+            }
+        }
+
+        // The method used to prcess messages from clients
+        private void ProcessMessages(Socket socket)
+        {
+            try
+            {
+                // Busy wait. Process received messages and check for messages to send to the client
+                while (true)
+                {
+                    // Receive any messages on the socket
+                    if (socket.Available > 0)
+                    {
+                        var bytes = new byte[socket.ReceiveBufferSize];
+                        var bytesRead = socket.Receive(bytes);
+
+                        // Convert to a string
+                        var dataReceived = Encoding.ASCII.GetString(bytes, 0, bytesRead);
+
+                        if (dataReceived.Equals(DISCOVER_MESSAGE))
+                        {
+                            outputTextBlock.Text += $"{DateTime.Now}: Received \"{dataReceived}\" from {socket.RemoteEndPoint}. Sending Discovery Message{Environment.NewLine}{Environment.NewLine}";
+
+
+                            //udpClient.Send(responseData, responseData.Length, endPoint);
+                        }
+                        else if (dataReceived.Equals(GET_DATA_MESSAGE))
+                        {
+                            outputTextBlock.Text += $"{DateTime.Now}: Received \"{dataReceived}\" from {socket.RemoteEndPoint}. Sending Data Message{Environment.NewLine}{Environment.NewLine}";
+
+                            //udpClient.Send(responseData, responseData.Length, endPoint);
+                        }
+                        else
+                        {
+                            outputTextBlock.Text += $"{DateTime.Now}: Received \"{dataReceived}\" from {socket.RemoteEndPoint}. Unrecognized message{Environment.NewLine}{Environment.NewLine}";
+                        }
+
+                        // Don't need to wait so busily. I can probably take this out, I don't think it solved the problem 
+                        // I was trying to fix, but I don't want to risk breaking anything else and 300ms is plently responsive
+                        //Thread.Sleep(300);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
             }
         }
     }
